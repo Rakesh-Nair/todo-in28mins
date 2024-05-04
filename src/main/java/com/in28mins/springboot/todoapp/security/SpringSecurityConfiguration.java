15638d14196cfe173fdf012c52af698bce56aaf2
Rakesh Nair <crucifyrakesh@gmail.com>
2024-05-04 22:24:25 +0530

    Add password encoder

---
+++ src/main/java/com/in28mins/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.in28mins.springboot.todoapp.security;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+	InMemoryUserDetailsManager manager;
+
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+		return new InMemoryUserDetailsManager(
+				User.builder().passwordEncoder(passwordEncoder )
+				.password("admin")
+				.username("admin")
+				.roles("USER", "ADMIN")
+				.build());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
