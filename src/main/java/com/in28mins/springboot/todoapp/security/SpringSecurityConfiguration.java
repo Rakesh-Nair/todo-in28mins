90a095b2889997ff87e3a88c447048f092fc8629
Rakesh Nair <crucifyrakesh@gmail.com>
2024-06-02 12:27:28 +0530

    Add new user

--- src/main/java/com/in28mins/springboot/todoapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28mins/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,13 +17,22 @@ public class SpringSecurityConfiguration {
 
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
+		
+		UserDetails userDetails = createNewUser("rakesh123", "rakesh");
+		UserDetails userDetails1 = createNewUser("rakesh123", "rishaan");
+		UserDetails userDetails2 = createNewUser("rakesh123", "ramita");
+		
+		return new InMemoryUserDetailsManager(userDetails, userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String password, String username) {
 		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-		return new InMemoryUserDetailsManager(
-				User.builder().passwordEncoder(passwordEncoder )
-				.password("rakesh123")
-				.username("rakesh")
-				.roles("USER", "ADMIN")
-				.build());
+		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder )
+		.password(password)
+		.username(username)
+		.roles("USER", "ADMIN")
+		.build();
+		return userDetails;
 	}
 	
 	@Bean
